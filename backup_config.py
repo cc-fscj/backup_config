023e1f5a5cba6a0384129f06d737ccd053fa4c84
cc-fscj <s3734607@students.fscj.edu>
2022-03-13 13:41:32 -0700

    version 1 backup file

---
+++ backup_config.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+#Device backup script
+
+#Import modules
+import datetime,pathlib,os,netmiko
+from netmiko import ConnectHandler
+from getpass import getpass
+
+#Gather credentials
+USERNAME = input('Username: ')
+PASSWORD = getpass('Password: ')
+
+#File information
+filename = 'csrBackup.txt'
+current_directory = pathlib.Path.cwd()
+open_action = 'w'
+backup_file = open(current_directory/filename,open_action)
+
+#Device information
+host = '192.168.49.254'
+device = {'ip' : host,'username' : USERNAME, 'password' : PASSWORD, 'device_type' : 'cisco_ios'}
+
+#Connect to device
+device_connection = ConnectHandler(**device)
+output = device_connection.send_command('show run')
+
+#Copy device output to file
+backup_file.write(output)
+backup_file.close()
